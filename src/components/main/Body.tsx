2c26eaf71b2346f15d3379273d2af1c728500bda
mohammad <mohammadghiasi005@gmail.com>
2024-05-15 13:09:08 +0330

    Skelton

--- src/components/main/Body.tsx
+++ src/components/main/Body.tsx
@@ -7,24 +7,21 @@ import { game } from '@/types/gametType';
 import GameCard from './cards/GameCard';
 import { log } from 'console';
 import Image from 'next/image';
+import CardSkelton from './cards/CardSkelton';
 
 export default function Body() {
     const { data, isPending, error, isError } = useGetData("/games")
-    
-
+    const skelton: number[] = [1,2,3,4,5,6,7,8,9,10]
     return (
         <>
             <Flex>
                 <Flex flex={{ base: "0", lg: "2" }}>
                     <VerticalNav />
                 </Flex>
-                <Flex flex={{ base: "12", lg: "10" }}>
-                    {(!data && isPending) && <Text>Loading</Text>}
+                <Flex flex={{ base: "12", lg: "10" }} justifyContent='center'>
                     <SimpleGrid columns={{sm: 1, md: 2, xl: 3}} spacing={18}>
-                        {data?.map((item: game) =>  <GameCard key={item?.id} game={item} />
-                        )}
-                        {/* <GameCard key={item?.id} id={item?.id} name={item?.name} image={item?.background_image} /> */}
-                        {/* <GameCard id={item.id} name={item.name} image={item.background_image} /> */}
+                        {isPending && skelton.map((item: number) => <CardSkelton key={item} />)}
+                        {data?.map((item: game) =>  <GameCard key={item?.id} game={item} />)}
                     </SimpleGrid>
                 </Flex>
             </Flex>
---
+++ src/components/main/cards/CardSkelton.tsx
@@ -0,0 +1,13 @@
+import { Card, Box, CardBody, SimpleGrid, Skeleton, SkeletonText } from '@chakra-ui/react'
+
+
+export default function CardSkelton() {
+    return (
+        <Card  width={{ base: "340px", md: "330px", lg: "320px" }}  overflow="hidden" height="300px" cursor="pointer">
+            <Skeleton startColor="gray.500" endColor="gray.500" height="300px" />
+            <CardBody className="mt-5 mx-3">
+                <SkeletonText startColor="gray.600" endColor="gray.500" height="80px" />
+            </CardBody>
+        </Card>
+    )
+}
